dd1fb4df3ecb10ab3766c5d0885fcb0e53ba2f9e
Arthur B <alkanife@gmail.com>
2023-03-06 14:28:44 +0100

    Update FakeMCServer.java

--- src/main/java/me/michidik/fakemcserver/FakeMCServer.java
+++ src/main/java/me/michidik/fakemcserver/FakeMCServer.java
@@ -18,7 +18,7 @@ import java.nio.file.Files;
 public class FakeMCServer {
 
     public static final String VERSION = "1.1";
-    public static final String URL = "https://github.com/alkanife-mc/FakeMCServer";
+    public static final String URL = "https://github.com/alkanife/FakeMCServer";
 
     private static Logger logger;
 
